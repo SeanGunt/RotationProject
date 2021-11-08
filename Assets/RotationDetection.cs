c5d80477ce27a37ac4859cf92b4a34c539b21688
SeanGunt <seangunterpackers@gmail.com>
2021-11-08 17:12:59 -0500

    Begin button working, and multiple xr interactions

--- Assets/RotationDetection.cs
+++ Assets/RotationDetection.cs
@@ -7,21 +7,16 @@ public class RotationDetection : XRSocketInteractor
 {
     public GameObject comparison;
     public override bool CanSelect(XRBaseInteractable args)
-    {
+    {   
         float angle = Quaternion.Angle(comparison.transform.rotation, args.gameObject.transform.rotation);
         Debug.Log(angle);
         return base.CanSelect(args) && angle<15;
-        
+
     }
-
-
-    // Start is called before the first frame update
     void Start()
     {
-        
-    }
 
-    // Update is called once per frame
+    }
     void Update()
     {
         
