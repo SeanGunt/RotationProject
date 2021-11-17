b49b7ff250c75d4778f714cc4a1f666cb0a5e00d
SeanGunt <SeanGunt@users.noreply.github.com>
2021-11-17 17:20:41 -0500

    Class_Wed_11/17/2021

--- Assets/Model_Grabbable_Interval.cs
+++ Assets/Model_Grabbable_Interval.cs
@@ -1,11 +1,13 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.XR.Interaction.Toolkit;
+using System.IO;
+using System;
 public class Model_Grabbable_Interval : XRGrabInteractable
 {
     public float stepSize = 15f;
     public static bool snapping = true;
-    
+
     [Tooltip("Match x rotation")]
     public bool matchX = false;
 
@@ -14,31 +16,50 @@ public class Model_Grabbable_Interval : XRGrabInteractable
 
     [Tooltip("Match z rotation")]
     public bool matchZ = false;
-
-    private void Awake()
+    public static DateTime startTime;
+    public static DateTime endTime;
+    public static bool started;
+    string filename;
+    public static int playerId = 0;
+    protected override void Awake()
     {
         base.Awake();
-    }
+        startTime = DateTime.Now;
+        if (!started)
+        {
+            started = true;
+            playerId = PlayerPrefs.GetInt("player", 0);
+            Debug.Log(SceneManager.GetActiveScene().name);
+            if (SceneManager.GetActiveScene().name != "RotationProject_Interval")
+            {
+                PlayerPrefs.SetInt("player", playerId + 1);
+            }
+        }
 
-    protected override void OnSelectExiting(XRBaseInteractor interactor) 
-    {
-        Debug.Log("Object dropped "+ RotationDetection.hovered + ", was successfully placed " + RotationDetection.correct);
     }
+    //protected override void OnSelectExiting(XRBaseInteractor interactor)
+    // {
+    //    Debug.Log("Object dropped " + RotationDetection.hovered + ", was successfully placed " + RotationDetection.correct);
+    //    File.AppendAllText(Application.dataPath.ToString() + Path.DirectorySeparatorChar + "data" + Path.DirectorySeparatorChar + playerId + ".csv",
+    //                playerId + "," +
+    //              SceneManager.GetActiveScene().name + "," + RotationDetection.correct + "," +
+    //               (DateTime.Now - startTime).TotalSeconds.ToString() + Environment.NewLine);
+    // }
 
     public override void ProcessInteractable(XRInteractionUpdateOrder.UpdatePhase arg)
     {
         base.ProcessInteractable(arg);
-        if(arg==XRInteractionUpdateOrder.UpdatePhase.Fixed && selectingInteractor != null) 
+        if (arg == XRInteractionUpdateOrder.UpdatePhase.Fixed && selectingInteractor != null)
         {
-            if (snapping == true) 
+            if (snapping == true)
             {
                 Vector3 newRotation = selectingInteractor.transform.rotation.eulerAngles;
-                newRotation.x = matchX ? Mathf.Round(newRotation.x/stepSize) * stepSize : newRotation.x;
-                newRotation.y = matchY ? Mathf.Round(newRotation.y/stepSize) * stepSize : newRotation.y;
-                newRotation.z = matchZ ? Mathf.Round(newRotation.z/stepSize) * stepSize : newRotation.z;
+                newRotation.x = matchX ? Mathf.Round(newRotation.x / stepSize) * stepSize : newRotation.x;
+                newRotation.y = matchY ? Mathf.Round(newRotation.y / stepSize) * stepSize : newRotation.y;
+                newRotation.z = matchZ ? Mathf.Round(newRotation.z / stepSize) * stepSize : newRotation.z;
                 transform.rotation = Quaternion.Euler(newRotation);
-            } 
-            else 
+            }
+            else
             {
                 transform.rotation = selectingInteractor.transform.rotation;
             }
